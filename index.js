73321dd8b4fd234668ef271e5efc64fb4c87d230
dscse23 <80138442+dscse23@users.noreply.github.com>
2022-08-01 02:15:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+    function verifyPassword() {
+        var pw = document.getElementById("pswd").value;
+        if (pw == "4281") {
+            document.getElementById("message").innerHTML = "Yay";
+            return true;
+        }
+    }
